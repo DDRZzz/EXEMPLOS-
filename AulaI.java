804e587fa12a35d06eee551bf42b065baea91a7c
Daniel Rodrigues Zuza <113559509+DDRZzz@users.noreply.github.com>
2023-08-18 17:18:32 -0300

    Create AulaI.java

---
+++ AulaI.java
@@ -0,0 +1,31 @@
+package com.mycompany.doctorzdevolta;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Aula1 {
+    //visibilidade
+    private int matricula, idade;
+    private String curso, RG;
+    private boolean autorizacao;
+}
+    
+    //Construtor com parãmetros (não tem retorno)
+    public Aula1(int matricula, int idade, String curso, String RG, boolean autorizacao){
+        this.matricula = matricula;
+        this.idade = idade;
+        this.curso = curso;
+        this.RG = RG;
+        this.autorizacao = autorizacao;
+    }
+
+    //Construtor vazio
+    public Aula1 (){
+
+}
+
+    //Métodos possuem retorno
+    public void imprimir (String idade){
+        this.idade 
+}
