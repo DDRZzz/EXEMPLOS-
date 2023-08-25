bca97d462a62ee43d0a987dc42850bdf3da15c7c
Daniel Rodrigues Zuza <113559509+DDRZzz@users.noreply.github.com>
2023-08-25 17:19:22 -0300

    Create exemplo2.java

---
+++ exemplo2.java
@@ -0,0 +1,18 @@
+package com.mycompany.doctorzdevolta;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Empregado {
+    
+    private String nome, sobrenome;
+    private double salario;
+    
+    public Empregado (String nome, String sobrenome, double salario){
+
+    this.nome = nome;
+    this.sobrenome = sobrenome;
+    this.salario = salario;
+}
+}
